008ddd7ceae8d3442636e234ff842d4199f9b910
Erkin Abdullaev <135661481+Erkin21@users.noreply.github.com>
2024-03-19 11:08:50 -0400

    Update modelTest.py

--- modelTest.py
+++ modelTest.py
@@ -5,23 +5,15 @@ import numpy as np
 interpreter = tflite.Interpreter(model_path='path/to/model.tflite')
 interpreter.allocate_tensors()
 
-# Get input details
-input_details = interpreter.get_input_details()
-input_shape = input_details[0]['shape']
-
-# Print input details
-print("Input shape:", input_shape)
-
 # Prepare input data (replace this with your actual input data)
-input_data = np.array([[165.31, 68.01, 25.05, 56, 87.07, 30.02, 32.69, 1.29, 0.91, 1.41, 59.5, 0.95, 0.51, 0.43, 2.49, 3.98, 2.31, 55.03, 43.08, 2.46, 7.59]], dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
+input_data = np.array([[165.31, 68.01, 25.05, 56, 87.07, 20.13, 30.02, 32.69, 1.29, 0.91, 1.41, 59.5, 0.95, 0.51, 0.43, 2.49, 3.98, 2.31, 55.03, 43.08, 2.46, 7.59]], dtype=np.float32)
+interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input_data)
 
 # Run inference
 interpreter.invoke()
 
 # Get the output results
-output_details = interpreter.get_output_details()
-output_data = interpreter.get_tensor(output_details[0]['index'])
+output_data = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
 
-# Process the output data as needed
-print("Inference Result:", output_data)
+# Print the output result
+print("Probability of having Alzheimer's:", output_data[0][1] * 100, "%")
