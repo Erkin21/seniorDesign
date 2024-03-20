65ea7e574684e4611679b2c0a4eed9b2c162ad09
Erkin Abdullaev <135661481+Erkin21@users.noreply.github.com>
2024-03-20 17:37:37 -0400

    Update modelTest.py

--- modelTest.py
+++ modelTest.py
@@ -1,25 +1,26 @@
+# !pip install tflite-runtime
 import numpy as np
 import tflite_runtime.interpreter as tflite
 
 # Load the TensorFlow Lite model
-interpreter = tflite.Interpreter(model_path='kerasModelUpdated.tflite')
+interpreter = tflite.Interpreter(model_path='//content//kerasModelUpdated1.tflite')
 interpreter.allocate_tensors()
 
 # Get input and output details
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Prepare input data (replace this with your actual input data)
-input_data = np.array([[165.31, 68.01, 25.05, 56, 87.07, 20.13, 30.02, 32.69, 1.29, 0.91, 1.41, 59.5, 0.95, 0.51, 0.43, 2.49, 3.98, 2.31, 55.03, 43.08, 2.46, 7.59]], dtype=np.float32)
+# Replace this by getting data from the excel sheet this value here is to just test to work it
+input_data = np.array([[157.44, 65.67, 22.03, 59.69, 93.03, 0.78, 29.32, 29.96, 1.34, 0.95, 1.4, 62.11, 1, 0.56, 0.42]], dtype=np.float32)
+print(input_data)
 
-# Set input tensor
+# Test the model with the input data
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
 # Run inference
 interpreter.invoke()
 
-# Get the output results
+# The function `get_tensor()` returns a copy of the tensor data.
 output_data = interpreter.get_tensor(output_details[0]['index'])
-
-# Print the output result
-print("Probability if they have Alz:", output_data[0][0] * 100, "%")
+probability_alz = (output_data[0] * 100).item()  
+print("Probability if they have Alzheimer's: {:.2f} %".format(probability_alz))
